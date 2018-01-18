442328c1afcdcf32bb06663271779c8c1caf7bcc
Persi <persi@sixsir.com>
2018-01-18 16:47:59 +0800

    Modifying constant nomenclature

--- env.php
+++ env.php
@@ -1,11 +1,9 @@
 <?php
 
-Class Env{
-    /**
-     * 当前php.ini中配置的env.name的值
-     */
-    const ENVIRON = 'product';
+// php.ini 中 env.name的值，默认值为product, 支持product(线上)、gray(灰度)、dev(开发)、local(本地),也支持自定义值
+define('ENV_ENVIRON', 'product');
 
+Class Env{
     /**
      * isProduct
      * @description 判断是否为线上
@@ -37,4 +35,12 @@ Class Env{
      * @return bool
      */
     public static function isLocal(){}
+
+    /**
+     * getEnviron
+     * @description 获取php.ini 中 env.name的值
+     * @version 1.0.0
+     * @return string
+     */
+    public static function getEnviron(){}
 }
