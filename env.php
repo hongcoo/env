54f872571d3f6f96379c44dbf8f0a9de748eb0c6
Persi <persi@sixsir.com>
2018-01-18 15:19:03 +0800

    add ide-help

--- env.php
+++ env.php
@@ -1,21 +1,40 @@
 <?php
-$br = (php_sapi_name() == "cli")? "":"<br>";
 
-if(!extension_loaded('env')) {
-	dl('env.' . PHP_SHLIB_SUFFIX);
+Class Env{
+    /**
+     * 当前php.ini中配置的env.name的值
+     */
+    const ENVIRON = 'product';
+
+    /**
+     * isProduct
+     * @description 判断是否为线上
+     * @version 1.0.0
+     * @return bool
+     */
+    public static function isProduct(){}
+
+    /**
+     * isGray
+     * @description 判断是否为灰度
+     * @version 1.0.0
+     * @return bool
+     */
+    public static function isGray(){}
+
+    /**
+     * isDev
+     * @description 判断是否为开发
+     * @version 1.0.0
+     * @return bool
+     */
+    public static function isDev(){}
+
+    /**
+     * isLocal
+     * @description 判断是否为本地
+     * @version 1.0.0
+     * @return bool
+     */
+    public static function isLocal(){}
 }
-$module = 'env';
-$functions = get_extension_funcs($module);
-echo "Functions available in the test extension:$br\n";
-foreach($functions as $func) {
-    echo $func."$br\n";
-}
-echo "$br\n";
-$function = 'confirm_' . $module . '_compiled';
-if (extension_loaded($module)) {
-	$str = $function($module);
-} else {
-	$str = "Module $module is not compiled into PHP";
-}
-echo "$str\n";
-?>
